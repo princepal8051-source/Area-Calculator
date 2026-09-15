a80b7ef96b63eb8c7198b539249618afe554b16d
princepal8051-source <princepal8051@gmail.com>
2026-09-15 15:55:38 +0530

    Updated project

--- cal.py
+++ cal.py
@@ -1,35 +1,75 @@
-print("****** AREA CALCULATOR ******")
+while True:
+    print("\n****** AREA CALCULATOR ******")
 
-print("""
+    print("""
 Press 1 to get the area of Square
 Press 2 to get the area of Circle
 Press 3 to get the area of Triangle
 Press 4 to get the area of Rectangle
+Press 5 to Exit
 """)
 
-choice = int(input("Enter a number between 1-4: "))
+    choice = int(input("Enter a number between 1-5: "))
 
-if choice == 1:
-    side = float(input("Enter the side of square: "))
-    area = side ** 2
-    print("Area of Square =", area)
+    if choice == 1:
+        side = float(input("Enter the side of square: "))
+        area = side ** 2
+        print("Area of Square =", area)
 
-elif choice == 2:
-    radius = float(input("Enter the radius of circle: "))
-    area = (22/7) * radius ** 2
-    print("Area of Circle =", area)
+    elif choice == 2:
+        radius = float(input("Enter the radius of circle: "))
+        area = (22/7) * radius ** 2
+        print("Area of Circle =", area)
 
-elif choice == 3:
-    base = float(input("Enter the base of triangle: "))
-    height = float(input("Enter the height of triangle: "))
-    area = 0.5 * base * height
-    print("Area of Triangle =", area)
+    elif choice == 3:
+        base = float(input("Enter the base of triangle: "))
+        height = float(input("Enter the height of triangle: "))
+        area = 0.5 * base * height
+        print("Area of Triangle =", area)
 
-elif choice == 4:
-    length = float(input("Enter the length of rectangle: "))
-    width = float(input("Enter the width of rectangle: "))
-    area = length * width
-    print("Area of Rectangle =", area)
+    elif choice == 4:
+        length = float(input("Enter the length of rectangle: "))
+        width = float(input("Enter the width of rectangle: "))
+        area = length * width
+        print("Area of Rectangle =", area)
 
-else:
-    print("Invalid Choice!")
+    elif choice == 5:
+        print("Thank you for using Area Calculator!")
+        break
+
+    else:
+        print("Invalid Choice!" \
+        "")
+
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+
+
+print("\nDo you want to continue? (y/n): ")
+
+
+
+print("\nDo you want to continue? (y/n): ")
+
+print("\nDo you want to continue? (y/n): ")
+
